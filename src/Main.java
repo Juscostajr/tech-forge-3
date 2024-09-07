eeb7f97cbf4e25191dec41c9ba244a14aa5e1517
Juscelino Fernandes da Costa Junior <juscostajr@gmail.com>
2024-09-06 21:41:48 -0300

    plantão tira duvidas

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
-import com.google.gson.Gson;
-import model.Cena;
-import model.Save;
-import repository.CenaDAO;
-import repository.SaveDAO;
+import controller.AntesDoJogoController;
 import spark.Spark;
 
 public class Main {
-    private static final Gson Gson = new Gson();
     public static void main(String[] args) {
-        try {
-            Save save = SaveDAO.novoJogo();
-            String saveJson = Gson.toJson(save);
-            Spark.get("/", (req,res) -> saveJson);
 
-            Spark.get("/cena/:id", (req,res) -> {
-                Integer cenaId = Integer.parseInt(req.params(":id"));
-                Cena cena = CenaDAO.findCenaById(cenaId);
-                return Gson.toJson(cena);
-            });
+        Spark.get("/:comando",new AntesDoJogoController());
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
