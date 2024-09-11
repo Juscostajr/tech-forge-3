c794793457293c0ec49f3d0aa956bdea12b77415
Juscelino Fernandes da Costa Junior <juscostajr@gmail.com>
2024-09-11 20:22:57 -0300

    aula 7

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 import com.google.gson.Gson;
-import model.Cena;
-import model.Save;
-import repository.CenaDAO;
-import repository.SaveDAO;
+import controller.AntesDoJogoController;
+import controller.DuranteOJogoController;
 import spark.Spark;
 
 public class Main {
-    private static final Gson Gson = new Gson();
+    private static final Gson gson = new Gson();
     public static void main(String[] args) {
-        try {
-            Save save = SaveDAO.novoJogo();
-            String saveJson = Gson.toJson(save);
-            Spark.get("/", (req,res) -> saveJson);
 
-            Spark.get("/cena/:id", (req,res) -> {
-                Integer cenaId = Integer.parseInt(req.params(":id"));
-                Cena cena = CenaDAO.findCenaById(cenaId);
-                return Gson.toJson(cena);
-            });
+        //Na classe main deixamos apenas as rotas da API
+        //controller -> É responsável por tratar as requisições e respostas http
+        //service -> É responsável pela lógica e regra de negócio da aplicação.
+        //model -> É responsável por definir os atributos e métodos das entidades do projeto.
+        //repository -> É responsável pela comunicação com o banco de dados.
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        //Rota 1 http://localhost:4567/{o comando vai aqui}
+        Spark.get("/:comando", new AntesDoJogoController(gson));
+
+        //Rota 2 http://localhost:4567/{comando}/{save}
+        Spark.get("/:comando/:save", new DuranteOJogoController(gson));
+
+        //Implemente mais rotas se precisar
+
     }
 }
